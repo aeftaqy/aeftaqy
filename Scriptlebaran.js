f9b472be154798dd5786fbe01668bc38338e6612
aeftaqy <140826670+aeftaqy@users.noreply.github.com>
2024-04-09 08:44:29 +0700

    Update Scriptlebaran.js

--- Scriptlebaran.js
+++ Scriptlebaran.js
@@ -84,8 +84,8 @@ function ngetik() {
     strings: ["" + katangetik], startDelay: 50, speed: 40, cursor: true,
     afterComplete: function(){
         kalimat.innerHTML = katangetik;
-        /*kalimatc.style="margin-top:20px;margin-bottom:10px";
-        setTimeout(ngetik2,300);*/
+        kalimatc.style="margin-top:20px;margin-bottom:10px";
+        setTimeout(ngetik2,300);
         /////////////////////////////
         setTimeout(tombol,300);
     },}).go();
@@ -104,8 +104,8 @@ function ngetik2() {
   }
 
   function otomatis() {kalimatc.style.transform="scale(.3)";setTimeout(function(){kalimatc.style.transform="scale(1)";},250);} 
-  function kemunculan(){otomatis();kalimatc.innerHTML = ktbwh2;setTimeout(kemunculan2,3000);} 
-  function kemunculan2(){otomatis();kalimatc.innerHTML = ktbwh3;setTimeout(kemunculan3,3000);} 
-  function kemunculan3(){otomatis();kalimatc.innerHTML = katangetik2;setTimeout(kemunculan,3000);}
+  function kemunculan(){otomatis();kalimatc.innerHTML = ktbwh2;setTimeout(kemunculan2,2000);} 
+  function kemunculan2(){otomatis();kalimatc.innerHTML = ktbwh3;setTimeout(kemunculan3,2000);} 
+  function kemunculan3(){otomatis();kalimatc.innerHTML = katangetik2;setTimeout(kemunculan,2000);}
   
        async function jawab(){await swals.fire('Kirim Dana nya, ya!');window.location = "https://api.whatsapp.com/send?phone=&text=" + pesanwhatsapp;}
